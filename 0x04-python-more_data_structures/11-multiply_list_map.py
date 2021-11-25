1219e3a767ce024675eb2172742c7accf822e678
Ephantus Mwangi <litephantus@gmail.com>
2021-11-25 04:26:46 +0300

    Update 11-multiply_list_map.py

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    """Return the weighted average of all integers in a list of tuples."""
+  
     if not isinstance(my_list, list) or len(my_list) == 0:
         return (0)
     avg = 0
