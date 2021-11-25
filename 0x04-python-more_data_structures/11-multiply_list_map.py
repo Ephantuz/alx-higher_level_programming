dbfa5754acc2c41b594f7fc31d2e6572e5661270
Ephantus Mwangi <litephantus@gmail.com>
2021-11-25 04:20:00 +0300

    update 100-multiply-list

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
-def multiply_list_map(my_list=[], number=0):
-    return list(map(lambda i: i * number, my_list))
+def weight_average(my_list=[]):
+    if my_list:
+        return sum(a * b for a, b in my_list) / sum(b for a, b in my_list)
+    return 0
+    # = ((1 * 2) + (2 * 1) + (3 * 10) + (4 * 2)) / (2 + 1 + 10 + 2)
